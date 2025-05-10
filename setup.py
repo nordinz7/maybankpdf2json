a47338eb9fa8cb750bd556d7483b2a48d4a69b82
Nordin <nordin.zahari@swiftlogistics.com.my>
2025-05-10 23:03:03 +0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="maybankpdf2json",
-    version="0.1.2",
+    version="0.1.3",
     author="Nordin",
     author_email="vipnordin@gmail.com",
     description="A package for extracting JSON data from Maybank account statements(PDF format).",
